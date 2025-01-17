1f796fdf05662426c7332b7915963bd6815633c0
KarimZaghloul88 <73558420+KarimZaghloul88@users.noreply.github.com>
2025-01-17 22:52:06 +0100

    Update Config.cs
    personal

--- TelegramReferralBot/VS/TelegramReferralBot/Config.cs
+++ TelegramReferralBot/VS/TelegramReferralBot/Config.cs
@@ -6,23 +6,23 @@ namespace TelegramReferralBot
 {
     public static class Config
     {
-        public static string BotAccessToken = "";
+        public static string BotAccessToken = "7310169896:AAEsP4OogGLxApIf3v1KtRskGV6_Gq8opa0";
 
         public static string outputFilePath = "";
 
-        public static string linkToGroup = "";
+        public static string linkToGroup = "https://t.me/agentzynk";
 
-        public static string linkToBot = "";
+        public static string linkToBot = "https://t.me/zynkreferral_bot";
 
-        public static string apw = "";
+        public static string apw = "666666";
 
-        public static string startDate = "";
+        public static string startDate = "1/17/2025";
 
-        public static int numberOfDays = 0;
+        public static int numberOfDays = 7;
 
-        public static int maxNumberOfPointsPerDayPerUser = 0;
+        public static int maxNumberOfPointsPerDayPerUser = 100;
 
-        public static int thresholdForMessagePoint = 0;
+        public static int thresholdForMessagePoint = 15;
 
         public static long groupChatIdNumber = 0;
     }
